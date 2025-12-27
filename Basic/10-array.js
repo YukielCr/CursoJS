86a3465dbf01a1601a3e31824690028fee835884
Yukiel <140352744+YukielCr@users.noreply.github.com>
2025-12-26 20:35:45 -0600

    10 array

---
+++ Basic/10-array.js
@@ -0,0 +1,22 @@
+// array
+
+//Declaracion
+let myarray=[]  //Este es ma utilizado
+let myarray2=new Array()
+
+console.log(myarray)
+console.log(myarray2)
+
+//Inicializacion 
+
+myarray=[3]
+myarray2=new Array(3)
+
+console.log(myarray)
+console.log(myarray2)
+
+myarray=[1,2,3,4,5,6,7,8,9]
+myarray2=new Array(3)
+
+console.log(myarray)
+console.log(myarray2)

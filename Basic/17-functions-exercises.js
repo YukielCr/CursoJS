eacdd13c902fbfc96160c8e0e46dd7d5bb59f28e
Yukiel <140352744+YukielCr@users.noreply.github.com>
2025-12-29 17:05:10 -0600

    16 frunciones

--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -7,10 +7,60 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=14146
 
 // 1. Crea una función que reciba dos números y devuelva su suma
 
+function suma(a, b) {
+    return a + b
+}
+
+//console.log(5+5)
+
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
 
-// 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
+myArray = [1, 998, 8, 2, 7, 3, 2, 5, 6, 888]
 
+function numberMayor(numeros) {
+    let numeroMayor = 0
+    let position = 0
+    for (let i = 0; i <= numeros.length; i++) {
+        //console.log(numeros[i]);
+        if (numeroMayor < numeros[i]) {
+            //console.log(numeros[i]);
+            numeroMayor = numeros[i]
+            position = i + 1
+        }
+    }
+    console.log(`El numero mayor es el de ${numeroMayor} en la pociocion ${position}`)
+}
+
+// numberMayor(myArray)
+
+myArray = [1, 998, 8, 2, 7, 3, 2, 5, 6, 888]
+function nuMayor(arre) {
+    let mayor = 0
+    let position = 0
+    for (let valor of myArray) {
+        if (mayor < valor) {
+            mayor = valor
+        }
+    }
+    console.log(`El numero mayor es el de ${mayor} en la pociocion ${position}`)
+}
+
+//nuMayor(myArray)
+
+
+// 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
+let mensa = "Hola Mundo"
+
+function vocales(men) {
+    let nVocales = 0
+    for (let char of men) {
+        console.log(char);
+        if(char.to==)
+    }
+}
+
+
+vocales(mensa)
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
 
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario

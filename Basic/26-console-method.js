2aa162980662aac75b7cf78b9080b59ca92ea769
Yukiel <140352744+YukielCr@users.noreply.github.com>
2026-01-03 21:46:01 -0600

    26 - console methods

--- Basic/26-console-method.js
+++ Basic/26-console-method.js
@@ -23,6 +23,63 @@ data=[
 
 data2=[
     {name:"Yuki",age:21},
-    {name:"AArlethna",age:21},
+    {name:"Arleth",age:21},
 ]
 console.table(data2);
+
+
+//group
+console.group("Grupo de mensajes");
+console.log("Mensaje 1");
+console.log("Mensaje 2");
+console.log("Mensaje 3");
+console.groupEnd("Grupo de mensajes");
+
+
+// time
+
+console.time("Tiempo de ejecución 2")
+
+for (let i = 0; i < 10000; i++) {
+
+}
+
+console.time("Tiempo de ejecución 1")
+
+for (let i = 0; i < 10000; i++) {
+
+}
+
+console.timeEnd("Tiempo de ejecución 2")
+
+for (let i = 0; i < 10000; i++) {
+
+}
+
+console.timeEnd("Tiempo de ejecución 1")
+
+// assert
+
+console.log("")
+let age=17
+console.assert(age>=18, "El usuario debe de ser mayor de edad")
+
+// count
+
+console.count("Click")
+console.count("Click")
+console.count("Click")
+console.countReset("Click")
+console.count("Click")
+
+// trace
+function a(){
+    b()
+}   
+function b(){
+    console.trace("Seguimiento de la ejecución");
+}
+
+a()
+
+console.clear()
